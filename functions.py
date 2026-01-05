39c437bf66db4097e68289e79a63ca3d2ab99be9
Masudur Rabbi Rahat <rabbirahat9@gmail.com>
2026-01-05 11:26:40 +0600

    add an assertion to convert_fahrenheit_to_celsius to reject temperatures below absolute zero

--- functions.py
+++ functions.py
@@ -8,4 +8,5 @@ def multiply(a, b):
     return a * b
 
 def convert_fahrenheit_to_celsius(fahrenheit):
-    return multiply(subtract(fahrenheit, 32), 5 / 9) # <-- Fix this in step 7
+    assert fahrenheit >= -459.67, "Temperature below absolute zero!"
+    return multiply(subtract(fahrenheit, 32), 5 / 9)
